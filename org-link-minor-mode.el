b466dd0db5182555b8b55529dc5cc4e056e8e178
Stefan-W. Hahn <stefan.hahn@s-hahn.de>
2017-06-15 17:52:42 +0200

    Correted adaption, silent byte compiler.

--- org-link-minor-mode.el
+++ org-link-minor-mode.el
@@ -1,8 +1,9 @@
 ;;; org-link-minor-mode.el --- Enable org-mode links in non-org modes
 ;;
-;; Copyright (C) 2012
+;; Copyright (C) 2012-2017
 ;; Author: Sean O'Halpin <sean dot ohalpin at gmail dot com>
-;; Package-Requires: ((org "7"))
+;; Changes for org v9: Stefan-W. Hahn <stefan dot hahn at s-hahn dot de>
+;; Package-Requires: ((org "8"))
 ;;
 ;; Enables org-mode links of the form:
 ;;
@@ -21,6 +22,18 @@
 
 (require 'org)
 
+;; Following declarations are necessary to make the byte compiler happy.
+
+;; For org v8 compatibility (if used with org v9)
+(declare-function org-activate-plain-links "org" (limit))
+(declare-function org-activate-angle-links "org" (limit))
+(declare-function org-activate-bracket-links "org" (limit))
+(declare-function org-decompose-region "org-compat" (beg end))
+
+;; For org v9 compatibility (if used with org v8)
+(declare-function org-activate-links "org" (limit))
+(declare-function org-activate-dates "org" (limit))
+
 (defun org-link-minor-mode-unfontify-region (beg end &optional maybe_loudly)
   "Remove fontification and activation overlays from links."
   (font-lock-default-unfontify-region beg end)
@@ -28,7 +41,9 @@
          (inhibit-read-only t) (inhibit-point-motion-hooks t)
          (inhibit-modification-hooks t)
          deactivate-mark buffer-file-name buffer-file-truename)
-    (org-decompose-region beg end)
+    (if (fboundp 'org-decompose-region)
+        (org-decompose-region beg end)
+      (decompose-region beg end))
     (remove-text-properties beg end
                             '(mouse-face t keymap t org-linked-text t
                                          invisible t intangible t
@@ -45,18 +60,26 @@
   "Toggle display of org-mode style bracket links in non-org-mode buffers."
   :lighter " org-link"
   :keymap org-link-minor-mode-map
-  (let (org-link-minor-mode-keywords)
+  (let ((lk org-highlight-links)
+        org-link-minor-mode-keywords)
     (if (fboundp 'org-activate-links)
         ;; from Org v9.2
         (setq org-link-minor-mode-keywords
               (list
-               'org-activate-links
-               '(org-activate-dates (0 'org-date t))
-               ))
+               '(org-activate-links)
+	       (when (memq 'tag lk) '(org-activate-tags (1 'org-tag prepend)))
+	       (when (memq 'radio lk) '(org-activate-target-links (1 'org-link t)))
+	       (when (memq 'date lk) '(org-activate-dates (0 'org-date t)))
+	       (when (memq 'footnote lk) '(org-activate-footnote-links))))
       (setq org-link-minor-mode-keywords
-            '(org-activate-angle-links (0 'org-link t))
-            '(org-activate-plain-links)
-            '(org-activate-bracket-links (0 'org-link t))))
+            (list
+	     (if (memq 'tag lk) '(org-activate-tags (1 'org-tag prepend)))
+	     (if (memq 'angle lk) '(org-activate-angle-links (0 'org-link t)))
+	     (if (memq 'plain lk) '(org-activate-plain-links (0 'org-link t)))
+	     (if (memq 'bracket lk) '(org-activate-bracket-links (0 'org-link t)))
+	     (if (memq 'radio lk) '(org-activate-target-links (0 'org-link t)))
+	     (if (memq 'date lk) '(org-activate-dates (0 'org-date t)))
+	     (if (memq 'footnote lk) '(org-activate-footnote-links)))))
     (if org-link-minor-mode
         (if (derived-mode-p 'org-mode)
             (progn
